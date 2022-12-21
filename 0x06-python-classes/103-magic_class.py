c7ff293aa267748394bcb101eab6050ffed44e65
Damilola010 <dhamedrah@gmail.com>
2022-12-21 10:57:54 +0000

    Python Classes

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -19,5 +19,8 @@ class MagicClass:
 
 	def area(self):
 		"""Return the area of the MagicClass."""
-		return (self.__radius ** 2 * math.pi) def circumference(self): """Return The circumference of the MagicClass."""
-		return (2 * math.pi * self.__radius)
+		return self.__radius ** 2 * math.pi
+
+	def circumference(self):
+		"""such docstring"""
+		return 2 * math.pi * self.__radius
